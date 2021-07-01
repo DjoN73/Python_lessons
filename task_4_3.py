2a32a018ddcbe1aed02cd3e365d8f1f7d92eea7a
DjoN73 <djon73@inbox.ru>
2021-07-01 21:45:09 +0300

    dz_4

---
+++ task_4_3.py
@@ -0,0 +1,17 @@
+from requests import get, utils
+from datetime import date
+
+
+def currency_rates(code):
+    responce = utils.get_unicode_from_response(get('http://www.cbr.ru/scripts/XML_daily.asp'))
+    content = responce.split('<Valute ID=')
+    d, m, y = content[0].split()[3].split('Date=')[1][1:-1].split('.')
+    for el in content:
+        if code.upper() in el:
+            print(date(year=int(y), month=int(m), day=int(d)))
+            return float(el.replace('/', '').split('<Value>')[-2].replace(',', '.'))
+
+
+if __name__ == '__main__':
+    print(currency_rates('usd'))
+    print(currency_rates('EUR'))
